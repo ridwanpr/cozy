f53bae80ba11dd65156817ac7fcd1b0e7ac80ca0
Ridwan <ridwanpr@outlook.com>
2024-10-17 01:45:51 +0700

    splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -8,50 +8,70 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 50,
-            left: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/logo.png'),
+        bottom: false,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset('assets/splash_image.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/logo.png'),
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'Find Cozy House \nto Stay and Happy',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Stop membuang banyak waktu\npada tempat yang tidak habitable',
+                    style: greyTextStyle.copyWith(fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: 210,
+                    height: 50,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: purpleColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(17),
+                        ),
+                      ),
+                      child: Text(
+                        'Explore now',
+                        style: whiteTextStyle.copyWith(fontSize: 18),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Find Cozy House \nto Stay and Happy',
-                style: blackTextStyle.copyWith(
-                  fontSize: 24,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Stop membuang banyak waktu\npada tempat yang tidak habitable',
-                style: greyTextStyle.copyWith(fontSize: 16),
-              ),
-              const SizedBox(height: 40,),
-              SizedBox(
-                width: 210,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('Explore now'),
-                ),
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
